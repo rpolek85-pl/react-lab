7d53dfff663a911d9580370b19ac4eef73439780
rpole <rpolek85@gamil.com>
2026-05-16 10:06:06 +0200

    email validation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
+import {useState} from "react";
+import './App.css';
+
 function App() {
-    let email = 'rpolek@student.agh.edu.pl';
+    const [email, setEmail] = useState('rpolek85@gmail.com');
+
+    let msg;
+    if(email.length < 6) {
+        msg = <div>Twój email jest za krótki</div>;}
+    else if(email.length > 20){
+        msg = <div>Twój email jest za długi</div>}
+    else {
+        msg = <div>Twój email jest ok</div>;}
+
 
     function handleChange(event) {
-        console.log(event.target.value);
+        setEmail(event.target.value);
+
+
     }
 
     return (
         <div>
             <h1>System do zapisów na zajęcia</h1>
             <h2>Twój e-mail to {email}.</h2>
-            <input type="text" onChange={handleChange}/>
+            {msg}
+            <input type="text" value={email} onChange={handleChange}/>
         </div>
     );
 }
